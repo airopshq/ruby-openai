c518ed2265b5c6b0d14243b7aa97b791adb398f1
Ana Esponda <ana@airops.com>
2023-06-15 13:58:40 -0300

    Support multi tenancy

--- lib/openai/client.rb
+++ lib/openai/client.rb
@@ -1,26 +1,28 @@
 module OpenAI
   class Client
+    attr_reader :access_token, :organization_id, :uri_base, :request_timeout
+
     def initialize(access_token: nil, organization_id: nil, uri_base: nil, request_timeout: nil)
-      OpenAI.configuration.access_token = access_token if access_token
-      OpenAI.configuration.organization_id = organization_id if organization_id
-      OpenAI.configuration.uri_base = uri_base if uri_base
-      OpenAI.configuration.request_timeout = request_timeout if request_timeout
+      @access_token = access_token || OpenAI.configuration.access_token
+      @organization_id = organization_id || OpenAI.configuration.organization_id
+      @uri_base = uri_base || OpenAI.configuration.uri_base
+      @request_timeout = request_timeout || OpenAI.configuration.request_timeout
     end
 
     def chat(parameters: {})
-      OpenAI::Client.json_post(path: "/chat/completions", parameters: parameters)
+      json_post(path: "/chat/completions", parameters: parameters)
     end
 
     def completions(parameters: {})
-      OpenAI::Client.json_post(path: "/completions", parameters: parameters)
+      json_post(path: "/completions", parameters: parameters)
     end
 
     def edits(parameters: {})
-      OpenAI::Client.json_post(path: "/edits", parameters: parameters)
+      json_post(path: "/edits", parameters: parameters)
     end
 
     def embeddings(parameters: {})
-      OpenAI::Client.json_post(path: "/embeddings", parameters: parameters)
+      json_post(path: "/embeddings", parameters: parameters)
     end
 
     def files
@@ -40,24 +42,24 @@ module OpenAI
     end
 
     def moderations(parameters: {})
-      OpenAI::Client.json_post(path: "/moderations", parameters: parameters)
+      json_post(path: "/moderations", parameters: parameters)
     end
 
     def transcribe(parameters: {})
-      OpenAI::Client.multipart_post(path: "/audio/transcriptions", parameters: parameters)
+      multipart_post(path: "/audio/transcriptions", parameters: parameters)
     end
 
     def translate(parameters: {})
-      OpenAI::Client.multipart_post(path: "/audio/translations", parameters: parameters)
+      multipart_post(path: "/audio/translations", parameters: parameters)
     end
 
-    def self.get(path:)
+    def get(path:)
       conn.get(uri(path: path)) do |req|
         req.headers = headers
       end
     end
 
-    def self.json_post(path:, parameters:)
+    def json_post(path:, parameters:)
       conn.post(uri(path: path)) do |req|
         req.headers = headers
         if parameters[:stream] && parameters[:on_data].is_a?(Proc)
@@ -68,37 +70,35 @@ module OpenAI
       end
     end
 
-    def self.multipart_post(path:, parameters: nil)
+    def multipart_post(path:, parameters: nil)
       conn.post(uri(path: path)) do |req|
         req.body = parameters
         req.headers = headers.merge({ "Content-Type" => "multipart/form-data" })
       end
     end
 
-    def self.delete(path:)
+    def delete(path:)
       conn.delete(uri(path: path)) do |req|
         req.headers = headers
       end
     end
 
-    private_class_method def self.conn
-      Faraday.new(params: nil, request: { timeout: OpenAI.configuration.request_timeout })
+    private
+
+    def conn
+      @conn ||= Faraday.new(params: nil, request: { timeout: request_timeout })
     end
 
-    private_class_method def self.uri(path:)
-      OpenAI.configuration.uri_base + OpenAI.configuration.api_version + path
+    def uri(path:)
+      uri_base + OpenAI.configuration.api_version + path
     end
 
-    private_class_method def self.headers
-      {
+    def headers
+      @headers ||= {
         "Content-Type" => "application/json",
-        "Authorization" => "Bearer #{OpenAI.configuration.access_token}",
-        "OpenAI-Organization" => OpenAI.configuration.organization_id
+        "Authorization" => "Bearer #{access_token}",
+        "OpenAI-Organization" => organization_id
       }
-    end
-
-    private_class_method def self.request_timeout
-      OpenAI.configuration.request_timeout
     end
   end
 end
